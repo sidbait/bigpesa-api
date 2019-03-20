4cfeb950cbf860d708d836a262e4c45085d0706a
rajeshsawant <rajesh.sawant@nazara.com>
2019-03-20 11:36:50 +0530

    new routes

--- app/routes/profileRoutes.js
+++ app/routes/profileRoutes.js
@@ -2,10 +2,11 @@ var webAppRoutes = express.Router();
 var authToken = require('../auth/auth');
 var userProfileController = require('../controller/userProfileController');
   
-
 webAppRoutes.post('/userProfile',authToken.validateAppReq, userProfileController.playerProfile)
 webAppRoutes.post('/followPlayer',authToken.validateAppReq,userProfileController.followPlayer);
 webAppRoutes.post('/unfollowPlayer',authToken.validateAppReq,userProfileController.unfollowPlayer);
+webAppRoutes.post('/followerList',authToken.validateAppReq,userProfileController.followerList);
+webAppRoutes.post('/followingList',authToken.validateAppReq,userProfileController.followingList);
 webAppRoutes.get('/newPlayerOnboard',authToken.validateAppReq, userProfileController.newPlayerOnboard);
 
 module.exports = webAppRoutes;
