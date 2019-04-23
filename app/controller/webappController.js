4c49e23c45e9956e08cd18a1703735fd7454715b
rajeshsawant <rajesh.sawant@nazara.com>
2019-04-23 17:17:43 +0530

    new

--- app/controller/webappController.js
+++ app/controller/webappController.js
@@ -3341,8 +3341,10 @@ function getNewLives(contestInfo,player_id,userToken,airpayToken,callback){
         console.log(debitResponse)
         console.log('NEW RESPONSE ---------------------------')
         if(debitResponse.statusCode == "200"){
+
             let query = ` update tbl_contest_players where used_lives = 0 
                             where player_id = ${playerId} and contest_id = ${event_id} `;
+                            console.log(query)
             dbConnection.executeQuery(query,"rmg_db",function(){});
             callback(err,debitResponse);
         }else{
