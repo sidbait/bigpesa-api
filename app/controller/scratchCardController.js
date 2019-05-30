c28e073bc92d18773f6c4d683bf7345ca79f2a2e
rajeshsawant <rajesh.sawant@nazara.com>
2019-05-30 17:33:11 +0530

    new

--- app/controller/scratchCardController.js
+++ app/controller/scratchCardController.js
@@ -95,7 +95,7 @@ module.exports = {
                     outJson.bonusCash = bonusCash;
                     outJson.paytmCash = paytmCash;
                     outJson.scratchCards = dbResult;
-                    console.log(dbResult[0])
+                    console.log(JSON.stringify(outJson))
                     sendResp.sendCustomJSON(null, req, res, true, outJson, "Success");
                 } else {
                     sendResp.sendCustomJSON(null, req, res, true, [], "No Data Found");
